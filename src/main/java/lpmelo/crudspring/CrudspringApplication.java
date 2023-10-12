a10acb7693dbd9022951cf71221c54bfa2e17b9c
lpmelo <lpmelo10@gmail.com>
2023-10-12 14:29:22 -0300

    initial commit

---
+++ src/main/java/lpmelo/crudspring/CrudspringApplication.java
@@ -0,0 +1,13 @@
+package lpmelo.crudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lpmelo/crudspring/CrudspringApplicationTests.java
@@ -0,0 +1,13 @@
+package lpmelo.crudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
